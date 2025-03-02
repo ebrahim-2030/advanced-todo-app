3398d3ff76f67faa461bcbebbc4a0be1cc562d5a
ebrahim <ebrahimasil@gmail.com>
2025-03-02 18:20:44 +0430

    Style: Update button styling for better UI

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -4,11 +4,11 @@ import { IoIosCheckmarkCircle } from "react-icons/io";
 
 const TodoItem = ({ todo, toggleTodo, deleteTodo }) => {
   return (
-    <li className="mt-2 flex items-center justify-between dark:bg-[#ffffff22] shadow border border-[#ccc8] dark:border-[#ffffff44] px-3 py-3 rounded-xl">
+    <li className="mt-2 flex items-center justify-between dark:bg-[#ffffff22] shadow border border-[#ddd] dark:border-[#ffffff44] px-3 py-3 rounded-xl">
       <div className="flex items-center gap-4">
         <button
           onClick={() => toggleTodo(todo.id)}
-          className="border-2  border-[#ffffff44] rounded-full h-6 w-6"
+          className="border-2 border-[#ccc8]  dark:border-[#ffffff44] rounded-full h-6 w-6"
         >
           {todo.completed ? (
             <IoIosCheckmarkCircle className="h-full w-full text-[#02BC7D]" />
