e1e4265639031ec1ab902c6f4ab4847dea55fcc8
ebrahim <ebrahimasil@gmail.com>
2025-03-02 05:34:50 +0430

    Created TodoList component

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import TodoItem from "./TodoItem";
 
-const TodoList = () => {
-  return <div>TodoList</div>;
+const TodoList = ({ todos, toggleTodo, deleteTodo }) => {
+  return (
+    <div>
+      <h2>All Todos</h2>
+
+      <ul className="w-full">
+        {todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            toggleTodo={toggleTodo}
+            deleteTodo={deleteTodo}
+          />
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default TodoList;
